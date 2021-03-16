3014f848c7b65705d6f0f12eee325518bf823dc0
cantalupo555 <cantalupo@protonmail.com>
2021-03-16 15:09:17 -0300

    resolved uri1016

---
+++ src/uri/uri1016.java
@@ -0,0 +1,15 @@
+package uri;
+
+import java.util.Scanner;
+
+public class uri1016 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int tempoDistancia = sc.nextInt() * 2;
+        sc.close();
+        System.out.println(tempoDistancia + " minutos");
+    }
+}
+
+// https://www.urionlinejudge.com.br/judge/problems/view/1016
