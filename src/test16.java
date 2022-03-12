7ceb5d6d265a22a647977ce2d5c8b88c7be154de
cantalupo555 <cantalupo@protonmail.com>
2022-03-11 23:19:24 -0300

    Sending exercises

---
+++ src/test16.java
@@ -0,0 +1,14 @@
+public class test16 {
+    public static void main(String[] args) {
+
+        for(int i=0; i<5; i++) {
+            System.out.println("Valor de i: " + i);
+        }
+
+        System.out.print("\n");
+
+        for(int i=4; i>=0; i--) {
+            System.out.println("Valor de i: " + i);
+        }
+    }
+}
