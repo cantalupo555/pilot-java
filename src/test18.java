0ec30bb02da0ca14358cee6094b37bd0ae950d74
cantalupo555 <cantalupo@protonmail.com>
2022-03-12 23:55:19 -0300

    Sending exercises

---
+++ src/test18.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class test18 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int N = sc.nextInt();
+        int[] vet = new int[N];
+
+        for (int i=0; i<N; i++) {
+            vet[i] = sc.nextInt();
+        }
+
+        for (int vetI : vet) {
+            if (vetI < 0) {
+                System.out.println(vetI);
+            }
+        }
+
+        sc.close();
+    }
+}
